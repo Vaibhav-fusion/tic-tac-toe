c66429947674c3f09ebd03d01ea606d9092c2dee
Vaibhav Singh <vishucodplayer@gmail.com>
2025-05-23 11:44:45 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Game from './component/Game'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <Game></Game>
   )
 }
 
