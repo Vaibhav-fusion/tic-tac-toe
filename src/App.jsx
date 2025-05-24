e743b2703033ba20a13b10cc37bdfdc866999f37
Vaibhav Singh <vishucodplayer@gmail.com>
2025-05-25 00:07:30 +0530

    console-check

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import Box from "./component/Box";
 import { useState } from "react";
 
+
+function WinnerCal(arr) {
+  const lines = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6],
+  ];
+  for (const [a,b,c] of lines)
+    if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c])
+      return arr[a];
+  return null;
+}
+
 export default function App() {
   const [squares, setSquare] = useState(Array(9).fill(null));
   const [isnext, setNext] = useState(true);
 
+  const winner = WinnerCal(squares)
+
+  const winner_text  = winner?winner:'No'
+
+  console.assert.log(winner)
+
   const handleClick = (idx) => {
     if (squares[idx]) {
-      return; //click on same cell ignore
+      return; //click on the same thingy 😭
     }
 
     const next = [...squares];
@@ -20,7 +39,7 @@ export default function App() {
   return (
     <main className="min-h-screen grid place-items-center bg-slate-100">
       <section className="grid grid-cols-3 gap-2">
-        {Array.from({ length: 9 }).map((val, i) => (
+        {squares.map((val, i) => (
           <Box key={i} value={val} onClick={()=>{ handleClick(i) }}  />
         ))}
       </section>
