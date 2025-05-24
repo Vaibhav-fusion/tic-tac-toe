453fcb86e6823aac761fe9a072005b8cc748d1ac
Vaibhav Singh <vishucodplayer@gmail.com>
2025-05-24 22:09:50 +0530

    basic-board

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-
 import Box from "./component/Box";
-
-
+import { useState } from "react";
 
 export default function App() {
+  const [squares, setSquare] = useState(Array(9).fill(null));
+  const [isnext, setNext] = useState(true);
+
+  const handleClick = (idx) => {
+    if (squares[idx]) {
+      return; //click on same cell ignore
+    }
+
+    const next = [...squares];
+    next[idx] = isnext ? "X" : "O";
+    setSquare(next);
+    setNext(!isnext);
+
+    
+  };
   return (
-
     <main className="min-h-screen grid place-items-center bg-slate-100">
-
-      <section className="grid grid-cols-3 gap-2">  
-        {Array.from({ length: 9 }).map((_, i) => (
-          <Box key={i} value={null} />
+      <section className="grid grid-cols-3 gap-2">
+        {Array.from({ length: 9 }).map((val, i) => (
+          <Box key={i} value={val} onClick={()=>{ handleClick(i) }}  />
         ))}
       </section>
+
+       <p className="mt-4 text-xl">
+        Next player: {isnext ? "X" : "O"}
+      </p>
     </main>
   );
 }
--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Box({value}) {
+function Box({value,onClick}) {
   return (
-    <button className='w-30 h-30 flex justify-center items-center text-3xl font-bold border-2 border-gray-400 bg-white'>{value}</button>
+    <button onClick={onClick} className='w-24 h-24 flex justify-center items-center text-3xl font-bold border-2 border-gray-400 bg-white'>{value}</button>
   )
 }
 
